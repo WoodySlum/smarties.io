6882337797d086d3b56fbfd79b1116c7103097c3
WoodySlum <seb.mizrahi@gmail.com>
2017-05-04 21:12:35 +0200

    Init services module

---
+++ services/Service.js
@@ -0,0 +1,30 @@
+"use strict";
+var Logger = require('./../logger/Logger');
+
+const STOPPED = 0;
+const RUNNING = 1;
+
+class Service {
+    constructor() {
+        this.status = STOPPED;
+    }
+
+    start() {
+        this.status = RUNNING;
+    }
+
+    stop() {
+        this.status = STOPPED;
+    }
+
+    restart() {
+        this.stop();
+        this.start();
+    }
+
+    status() {
+        return this.status;
+    }
+}
+
+module.exports = {Service:Service, STOPPED:STOPPED, RUNNING:RUNNING};
---
+++ services/webservices/WebServices.js
@@ -0,0 +1,39 @@
+"use strict";
+var Logger = require('./../../logger/Logger');
+var Service = require('./../Service').Service;
+var ServiceConst = require('./../Service');
+
+class WebServices extends Service {
+
+    constructor(port = 8080) {
+        super();
+        this.port = port;
+        let express = require('express');
+        this.app = express();
+        this.server = null;
+    }
+
+    start() {
+        if (this.status != ServiceConst.RUNNING) {
+            this.app.get('/services/', function(req, res) {
+                res.send('Hello\n');
+            });
+            this.server = this.app.listen(this.port);
+            Logger.info("Services are listening on port " + this.port);
+
+            super.start();
+        } else {
+            Logger.warn("Web services are already running");
+        }
+    }
+
+    stop() {
+        if (this.server && this.status == ServiceConst.RUNNING) {
+            this.server.close();
+        } else {
+            Logger.warn("Web services are already running");
+        }
+    }
+}
+
+module.exports = WebServices;
