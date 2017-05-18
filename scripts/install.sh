3302acfff4c7e1877e3aaded365e7543cb6dad2b
WoodySlum <seb.mizrahi@gmail.com>
2017-05-18 19:54:30 +0200

    Give a try on deploy script

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,6 +2,8 @@
 INSTALLATION_FOLDER="/var/hautomation/"
 sudo systemctl stop hautomation
 sudo cp -f ./hautomation.service /etc/systemd/system
+sudo systemctl daemon-reload
 sudo cp -Rf ../* $INSTALLATION_FOLDER
 sudo systemctl enable hautomation
 sudo systemctl start hautomation
+sudo rm -Rf $INSTALLATION_FOLDER/scripts
