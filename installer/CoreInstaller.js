b00c17efb9ffe41b87d0e7d253339871f951edb1
WoodySlum <seb.mizrahi@gmail.com>
2017-11-08 19:04:34 +0100

    Added ESP8266 stage platform installation command

--- installer/CoreInstaller.js
+++ installer/CoreInstaller.js
@@ -6,10 +6,12 @@ module.exports = {install:(installationManager) => {
     installationManager.register("0.0.3", ["x32", "x64"], "brew install graphicsmagick", false, false, true);
     installationManager.register("0.0.3", ["x32", "x64"], "brew install libav", false, false, true);
     installationManager.register("0.0.3", ["x32", "x64"], "pip install -U platformio", false, false, true);
+    installationManager.register("0.0.3", ["x32", "x64"], "pio platform install https://github.com/platformio/platform-espressif8266.git#feature/stage", false, false, true);
 
     // Raspberry Pi
     installationManager.register("0.0.3", ["arm", "arm64"], "apt-get update", true, true);
     installationManager.register("0.0.3", ["arm", "arm64"], "apt-get install -y imagemagick graphicsmagick", true, true);
     installationManager.register("0.0.3", ["arm", "arm64"], "apt-get install -y libav-tools", true, true);
     installationManager.register("0.0.3", ["arm", "arm64"], "pip install -U platformio", true, true);
+    installationManager.register("0.0.3", ["arm", "arm64"], "pio platform install https://github.com/platformio/platform-espressif8266.git#feature/stage", true, true);
 }};
