775917ad180053e7e58756a3562fa515103a18fa
WoodySlum <seb.mizrahi@gmail.com>
2018-01-04 20:01:30 +0100

    Fix lint issues

--- src/services/webservices/WebServices.js
+++ src/services/webservices/WebServices.js
@@ -88,16 +88,15 @@ class WebServices extends Service.class {
             this.app.use(BodyParser.json({limit: "2mb"}));
 
             const allowCrossDomain = function(req, res, next) {
-                res.header('Access-Control-Allow-Origin', '*');
-                res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-                res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+                res.header("Access-Control-Allow-Origin", "*");
+                res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+                res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
 
                 // intercept OPTIONS method
                 if ("OPTIONS" == req.method) {
-                  res.send(200);
-                }
-                else {
-                  next();
+                    res.send(200);
+                } else {
+                    next();
                 }
             };
 
