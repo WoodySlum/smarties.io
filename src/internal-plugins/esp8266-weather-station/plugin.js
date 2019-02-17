82a3c1dd35f839c9df5fe7dc2feebbecf43e6acc
WoodySlum <seb.mizrahi@gmail.com>
2019-02-17 19:21:04 +0100

    Change weather station to deep sleep

--- src/internal-plugins/esp8266-weather-station/plugin.js
+++ src/internal-plugins/esp8266-weather-station/plugin.js
@@ -11,7 +11,7 @@ function loaded(api) {
     api.init();
 
     const espPlugin = api.getPluginInstance("esp8266");
-    api.iotAPI.registerApp("app", "esp8266-weather-station", "ESP8266 Weather station", 2, api.iotAPI.constants().PLATFORMS.ESP8266, api.iotAPI.constants().BOARDS.NODEMCU, api.iotAPI.constants().FRAMEWORKS.ARDUINO, ["esp8266"], espPlugin.generateOptions(espPlugin.constants().MODE_SLEEP, REFRESH_TIME));
+    api.iotAPI.registerApp("app", "esp8266-weather-station", "ESP8266 Weather station", 2, api.iotAPI.constants().PLATFORMS.ESP8266, api.iotAPI.constants().BOARDS.NODEMCU, api.iotAPI.constants().FRAMEWORKS.ARDUINO, ["esp8266"], espPlugin.generateOptions(espPlugin.constants().MODE_DEEP_SLEEP, REFRESH_TIME));
 
     /**
      * This class manage EspWeatherStation
