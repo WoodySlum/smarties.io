0d537e9f407a90d39fc2630cb1debb6959933db1
WoodySlum <seb.mizrahi@gmail.com>
2019-07-08 20:20:43 +0200

    IFTTT plugin : first implementation, added a trigger scenario URL

---
+++ src/modules/pluginsmanager/publicapis/GatewayAPI.js
@@ -0,0 +1,41 @@
+"use strict";
+const PrivateProperties = require("./../PrivateProperties");
+
+/**
+ * Public API for gateway
+ * @class
+ */
+class GatewayAPI {
+    /* eslint-disable */
+    // /**
+    //  * Constructor
+    //  *
+    //  * @param  {GatewayManager} gatewayManager The gateway manager instance
+    //  * @returns {GatewayAPI}             The instance
+    //  */
+    constructor(gatewayManager) {
+        PrivateProperties.createPrivateState(this);
+        PrivateProperties.oprivate(this).gatewayManager = gatewayManager;
+    }
+    /* eslint-enable */
+
+    /**
+     * Get full hautomation URL
+     * 
+     * @returns {string} The URL
+     */
+    getDistantUrl() {
+        return PrivateProperties.oprivate(this).gatewayManager.getDistantUrl();
+    }
+
+    /**
+     * Get full hautomation API URL
+     *
+     * @returns {string} The URL
+     */
+    getDistantApiUrl() {
+        return PrivateProperties.oprivate(this).gatewayManager.getDistantApiUrl();
+    }
+}
+
+module.exports = {class:GatewayAPI};
