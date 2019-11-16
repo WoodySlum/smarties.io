10587c37c71a9b196b978bed8ded715c987d6797
WoodySlum <seb.mizrahi@gmail.com>
2019-11-16 21:15:33 +0100

    Added schema for weather station

--- src/internal-plugins/esp8266-weather-station/plugin.js
+++ src/internal-plugins/esp8266-weather-station/plugin.js
@@ -12,7 +12,20 @@ function loaded(api) {
 
     const espPlugin = api.getPluginInstance("esp8266");
     const wiringSchema = api.iotAPI.getWiringSchemaForLib("esp8266");
+    wiringSchema.right["D3"].push("DHT22 pin #2");
+    wiringSchema.left["3V3"].push("DHT22 pin #1");
+    wiringSchema.left["GND-1"].push("DHT22 pin #4");
+    wiringSchema.right["3V3-1"].push("BM180 VIN");
+    wiringSchema.right["GND-1"].push("BM180 GND");
+    wiringSchema.right["3V3-2"].push("Water Sensor pin + / VIN");
+    wiringSchema.right["GND-2"].push("Water Sensor pin - / GND");
+    wiringSchema.right["D4"].push("BM180 SDA");
+    wiringSchema.right["D5"].push("BM180 SCL");
+    wiringSchema.left["A0"].push("Water Sensor pin S / Data");
     api.iotAPI.registerApp("app", "esp8266-weather-station", "Nodemcu Weather station", 4, api.iotAPI.constants().PLATFORMS.ESP8266, api.iotAPI.constants().BOARDS.NODEMCU, api.iotAPI.constants().FRAMEWORKS.ARDUINO, ["esp8266"], espPlugin.generateOptions(espPlugin.constants().MODE_LIGHT_SLEEP, REFRESH_TIME), wiringSchema);
+    api.iotAPI.addIngredientForReceipe("esp8266-weather-station", "bmp180", "Pressure, temperature and altitude sensor", 1, true);
+    api.iotAPI.addIngredientForReceipe("esp8266-weather-station", "dht22", "Pressure, temperature sensor", 1, true);
+    api.iotAPI.addIngredientForReceipe("esp8266-weather-station", "Funduino water sensor", "Rain drop module", 1, true);
 
     /**
      * This class manage EspWeatherStation
