faa0fec9a8bb964a01024ecd647a391166a6e612
WoodySlum <seb.mizrahi@gmail.com>
2020-01-10 20:39:28 +0100

    Update to node 13.6

--- docker/hautomation/build-publish.sh
+++ docker/hautomation/build-publish.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
+
+PACKAGE_VERSION=$(cat ../../package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
+
 docker login
 DOCKER_ACC="woodyslum"
 DOCKER_REPO="hautomation"
-IMG_TAG="0.0.1"
+IMG_TAG="$PACKAGE_VERSION"
 
-docker build -t $DOCKER_ACC/$DOCKER_REPO:$IMG_TAG .
-# sudo docker push $DOCKER_ACC/$DOCKER_REPO:$IMG_TAG
+docker buildx create --use
+docker buildx build --push --platform linux/amd64,linux/arm/v7 -t $DOCKER_ACC/$DOCKER_REPO:$IMG_TAG .
+# docker build -t $DOCKER_ACC/$DOCKER_REPO:$IMG_TAG .
+# docker push $DOCKER_ACC/$DOCKER_REPO:$IMG_TAG
