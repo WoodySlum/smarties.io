3970776e99b00de78141e1850187d2c33a3243fd
WoodySlum <seb.mizrahi@gmail.com>
2020-01-15 22:28:25 +0100

    Update docker sh

--- docker/ci/build-publish.sh
+++ docker/ci/build-publish.sh
@@ -10,6 +10,7 @@ docker kill $(docker ps -q)
 # Clean images
 docker image rm -f $(docker image ls -a -q "*/hautomation-ci")
 docker system prune --all -f
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker buildx create --use --append --name hautomation-ci
 docker buildx build --platform linux/amd64,linux/arm/v7 --push -t $DOCKER_ACC/$DOCKER_REPO:$IMG_TAG -f Dockerfile .
 docker system prune --all -f
