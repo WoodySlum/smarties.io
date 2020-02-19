44242666b50cb79f152ae94d8039aca899b15ddf
WoodySlum <seb.mizrahi@gmail.com>
2020-02-19 20:43:07 +0100

    Tests on opencv

--- src/modules/aimanager/AiManager.js
+++ src/modules/aimanager/AiManager.js
@@ -2,6 +2,8 @@
 
 const bayes = require("bayes");
 const fs = require("fs");
+const path = require("path");
+const cv = require("opencv4nodejs");
 const Logger = require("./../../logger/Logger");
 const DateUtils = require("./../../utils/DateUtils");
 const TimeEventService = require("./../../services/timeeventservice/TimeEventService");
@@ -13,6 +15,7 @@ const CLASS_MONTHS = ["january", "february", "march", "april", "may", "june", "j
 const CLASS_DAYOFF = ["workon", "workoff"];
 const CLASS_DAYNIGHT = ["day", "night"];
 const CLASS_TIME = "time";
+
 
 /**
  * This class is used for artificial intelligence and machine learning
@@ -59,6 +62,111 @@ class AiManager {
         this.timeEventService.register(() => {
             self.saveClassifiers();
         }, this, TimeEventService.EVERY_HOURS_INACCURATE);
+
+        // const image = cv.imread("1581774480.JPG");
+        // const classifier = new cv.CascadeClassifier(cv.HAAR_FRONTALCATFACE);
+        // const { objects, numDetections } = classifier.detectMultiScale(image.bgrToGray());
+        //
+        // console.log('faceRects:', objects);
+        // console.log('confidences:', numDetections);
+        // // process.exit(0);
+        //
+        // const drawRect = (image, rect, color, opts = { thickness: 2 }) =>
+        //   image.drawRectangle(
+        //     rect,
+        //     color,
+        //     opts.thickness,
+        //     cv.LINE_8
+        //   );
+        //  const drawBlueRect = (image, rect, opts = { thickness: 2 }) => drawRect(image, rect, new cv.Vec(255, 0, 0), opts);
+        // const numDetectionsTh = 10;
+        // objects.forEach((rect, i) => {
+        //     // if (numDetections[i] > 20) {
+        //         const thickness = 3;//numDetections[i] < numDetectionsTh ? 1 : 2;
+        //         drawBlueRect(image, rect, { thickness });
+        //     // }
+        //
+        // });
+        // // cv.imshowWait('face detection', image);
+        // cv.imwrite("mat.png", image);
+
+        const grabFrames = (videoFile, delay, onFrame) => {
+  const cap = new cv.VideoCapture(videoFile);
+  let done = false;
+  const intvl = setInterval(() => {
+    let frame = cap.read();
+    // loop back to start on end of stream reached
+    if (frame.empty) {
+      cap.reset();
+      frame = cap.read();
+    }
+    onFrame(frame);
+
+    const key = cv.waitKey(delay);
+    done = key !== -1 && key !== 255;
+    if (done) {
+      clearInterval(intvl);
+      console.log('Key pressed, exiting.');
+    }
+  }, 0);
+};
+
+    const drawRectAroundBlobs = (binaryImg, dstImg, minPxSize, fixedRectWidth) => {
+  const {
+    centroids,
+    stats
+  } = binaryImg.connectedComponentsWithStats();
+let detected = 0;
+  // pretend label 0 is background
+  for (let label = 1; label < centroids.rows; label += 1) {
+    const [x1, y1] = [stats.at(label, cv.CC_STAT_LEFT), stats.at(label, cv.CC_STAT_TOP)];
+    const [x2, y2] = [
+      x1 + (fixedRectWidth || stats.at(label, cv.CC_STAT_WIDTH)),
+      y1 + (fixedRectWidth || stats.at(label, cv.CC_STAT_HEIGHT))
+    ];
+    const size = stats.at(label, cv.CC_STAT_AREA);
+    const blue = new cv.Vec(255, 0, 0);
+    if (minPxSize < size) {
+        detected++;
+      dstImg.drawRectangle(
+        new cv.Point(x1, y1),
+        new cv.Point(x2, y2),
+        { color: blue, thickness: 2 }
+      );
+    }
+  }
+  return detected;
+};
+
+        const bgSubtractor = new cv.BackgroundSubtractorMOG2();
+        const delay = 300;
+        grabFrames('https://webcam1.lpl.org/axis-cgi/mjpg/video.cgi', delay, (frame) => {
+            const foreGroundMask = bgSubtractor.apply(frame);
+
+            const iterations = 5;
+            const dilated = foreGroundMask.dilate(
+              cv.getStructuringElement(cv.MORPH_ELLIPSE, new cv.Size(4, 4)),
+              new cv.Point(-1, -1),
+              iterations
+            );
+            const blurred = dilated.blur(new cv.Size(10, 10));
+            const thresholded = blurred.threshold(200, 255, cv.THRESH_BINARY);
+
+            const minPxSize = 10000;
+            let detected = drawRectAroundBlobs(thresholded, frame, minPxSize);
+            if (detected > 1) {
+                console.log(detected);
+            }
+
+
+            cv.imshow('foreGroundMask', foreGroundMask);
+            cv.imshow('thresholded', thresholded);
+            cv.imshow('frame', frame);
+          cv.imwrite("mona-mat.png", frame);
+        });
+
+        // process.exit(0);
+        // cv.imwrite("/Users/smizrahi/Downloads/mona-mat.png", mat);
     }
 
     /**
