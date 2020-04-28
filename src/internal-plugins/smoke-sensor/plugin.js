5823c3a95b99404a521e1c52e4a9c4a042862e41
WoodySlum <seb.mizrahi@gmail.com>
2020-04-28 19:05:50 +0200

    Update smoke sensor icon

--- src/internal-plugins/smoke-sensor/plugin.js
+++ src/internal-plugins/smoke-sensor/plugin.js
@@ -39,7 +39,7 @@ function loaded(api) {
          * @returns {SmokeSensor}                                                       The instance
          */
         constructor(api, id, configuration) {
-            super(api, id, "SMOKE", configuration, api.exported.Icons.class.list()["fire_extinguisher"], 0);
+            super(api, id, "SMOKE", configuration, api.exported.Icons.class.list()["fire-station-1"], 0);
             this.chartType = api.exported.Sensor.constants().CHART_TYPE_BAR;
             this.aggregationMode = api.exported.Sensor.constants().AGGREGATION_MODE_MAX;
             this.unit = api.translateAPI.t("smoke.unit.tick");
