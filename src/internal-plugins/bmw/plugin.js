8a54aa1c5ed6d2b375ea5e0606eea470cde78fc5
WoodySlum <seb.mizrahi@gmail.com>
2020-06-16 19:16:12 +0200

    Improve bmw plugin

--- src/internal-plugins/bmw/plugin.js
+++ src/internal-plugins/bmw/plugin.js
@@ -80,7 +80,8 @@ function loaded(api) {
         constructor(api) {
             this.api = api;
             this.updateCarInfos();
-            this.previousChargingStatus = "";
+            this.previousChargingStatus = {};
+            this.carPictures = {};
 
             this.api.configurationAPI.setUpdateCb(() => {
                 this.updateCarInfos();
@@ -91,6 +92,23 @@ function loaded(api) {
                     self.updateCarInfos();
                 }
             }, this, this.api.timeEventAPI.constants().EVERY_MINUTES);
+        }
+
+        /**
+         * Update car picture
+         *
+         * @param  {Vehicle} vehicle           The vehicle
+         */
+        updateCarPicture(vehicle) {
+            vehicle.getImage().then((pic) => {
+                if (pic) {
+                    this.carPictures[vehicle.originalData.vin] = pic.split(",")[1];
+                    this.previousChargingStatus[vehicle.originalData.vin] = "";
+                    this.updateCarInfos();
+                }
+            }).catch((e) => {
+                api.exported.Logger.err(e.message);
+            });
         }
 
         /**
@@ -108,59 +126,75 @@ function loaded(api) {
                     ConnectedDriveApi.getVehicles().then((vehicles) => {
                         if (vehicles.length > 0) {
                             for (let i = 0 ; i < vehicles.length ; i++) {
-                                vehicles[i].getStatus().then((status) => {
-                                    // Total
-                                    const miniTiles = [];
+                                // Get picture
+                                if (!this.carPictures[vehicles[i].originalData.vin]) {
+                                    this.updateCarPicture(vehicles[i]);
+                                } else {
+                                    vehicles[i].getStatus().then((status) => {
+                                        // Total
+                                        const miniTiles = [];
 
-                                    miniTiles.push({icon: api.exported.Icons.class.list()["cab"], text: vehicles[i].model.split(" ")[0], colorDefault: api.themeAPI.constants().DARK_COLOR_KEY});
+                                        miniTiles.push({icon: api.exported.Icons.class.list()["cab"], text: vehicles[i].model.split(" ")[0], colorDefault: api.themeAPI.constants().DARK_COLOR_KEY});
 
-                                    if (status.remainingRangeTotal != null && status.remainingRangeTotal != "undefined") {
-                                        miniTiles.push({icon: api.exported.Icons.class.list()["fuel"], text: status.remainingRangeTotal + " km", colorDefault: api.themeAPI.constants().DARK_COLOR_KEY});
-                                    }
+                                        if (status.remainingRangeTotal != null && status.remainingRangeTotal != "undefined") {
+                                            miniTiles.push({icon: api.exported.Icons.class.list()["fuel"], text: status.remainingRangeTotal + " km", colorDefault: api.themeAPI.constants().DARK_COLOR_KEY});
+                                        }
 
-                                    if (status.chargingLevelHv != null && status.chargingLevelHv != "undefined") {
-                                        let icon = api.exported.Icons.class.list()["battery-2"];
-                                        if (status.chargingStatus == "CHARGING") {
-                                            icon = api.exported.Icons.class.list()["plug-1"];
-                                        } else if (status.chargingLevelHv < 25) {
-                                            icon = api.exported.Icons.class.list()["battery-0"];
-                                        } else if (status.chargingLevelHv < 50) {
-                                            icon = api.exported.Icons.class.list()["battery-1"];
-                                        } else if (status.chargingLevelHv < 75) {
-                                            icon = api.exported.Icons.class.list()["battery-2"];
-                                        } else if (status.chargingLevelHv < 95) {
-                                            icon = api.exported.Icons.class.list()["battery-3"];
+                                        if (status.chargingLevelHv != null && status.chargingLevelHv != "undefined") {
+                                            let icon = api.exported.Icons.class.list()["battery-2"];
+                                            if (status.chargingStatus == "CHARGING") {
+                                                icon = api.exported.Icons.class.list()["plug-1"];
+                                            } else if (status.chargingLevelHv < 25) {
+                                                icon = api.exported.Icons.class.list()["battery-0"];
+                                            } else if (status.chargingLevelHv < 50) {
+                                                icon = api.exported.Icons.class.list()["battery-1"];
+                                            } else if (status.chargingLevelHv < 75) {
+                                                icon = api.exported.Icons.class.list()["battery-2"];
+                                            } else if (status.chargingLevelHv < 95) {
+                                                icon = api.exported.Icons.class.list()["battery-3"];
+                                            } else {
+                                                icon = api.exported.Icons.class.list()["battery-4"];
+                                            }
+
+                                            miniTiles.push({icon: icon, text: status.chargingLevelHv + "%" + (status.remainingRangeElectric ? " [" + status.remainingRangeElectric + " km]" : ""), colorDefault: api.themeAPI.constants().DARK_COLOR_KEY});
+
+                                            // Send notifications
+                                            if (this.previousChargingStatus[vehicles[i].originalData.vin] == "CHARGING" && status.chargingStatus != "CHARGING") {
+                                                api.messageAPI.sendMessage("*", api.translateAPI.t("bmw.charged.message", vehicles[i].model));
+                                            }
+
+                                            this.previousChargingStatus[vehicles[i].originalData.vin] = status.chargingStatus;
+                                        }
+
+                                        // Closed status
+                                        if (status.doorLockState == "UNLOCKED" || status.doorDriverFront != "CLOSED" || status.doorDriverRear != "CLOSED" || status.doorPassengerFront != "CLOSED" || status.doorPassengerRear != "CLOSED" || status.hood != "CLOSED" || status.windowDriverFront != "CLOSED" || status.windowDriverRear != "CLOSED" || status.windowPassengerFront != "CLOSED" || status.windowPassengerRear != "CLOSED") {
+                                            miniTiles.push({icon: api.exported.Icons.class.list()["unlock"], text: api.translateAPI.t("bmw.unlocked"), colorDefault: api.themeAPI.constants().OFF_COLOR_KEY});
                                         } else {
-                                            icon = api.exported.Icons.class.list()["battery-4"];
+                                            miniTiles.push({icon: api.exported.Icons.class.list()["lock"], text: api.translateAPI.t("bmw.locked"), colorDefault: api.themeAPI.constants().DARK_COLOR_KEY});
                                         }
 
-                                        miniTiles.push({icon: icon, text: status.chargingLevelHv + " %" + (status.remainingRangeElectric ? " [" + status.remainingRangeElectric + " km]" : ""), colorDefault: api.themeAPI.constants().DARK_COLOR_KEY});
-
-                                        // Send notifications
-                                        if (this.previousChargingStatus == "CHARGING" && status.chargingStatus != "CHARGING") {
-                                            api.messageAPI.sendMessage("*", api.translateAPI.t("bmw.charged.message", vehicles[i].model));
+                                        // Health
+                                        if (status.conditionBasedServicingData) {
+                                            let health = true;
+                                            status.conditionBasedServicingData.forEach((conditionBasedServicingData) => {
+                                                if (conditionBasedServicingData.state != "OK") {
+                                                    health = false;
+                                                }
+                                            });
+                                            miniTiles.push({icon: api.exported.Icons.class.list()["wrench-1"], text: (health ? api.translateAPI.t("bmw.health.ok") : api.translateAPI.t("bmw.health.ko")), colorDefault: (health ? api.themeAPI.constants().DARK_COLOR_KEY : api.themeAPI.constants().OFF_COLOR_KEY)});
                                         }
 
-                                        this.previousChargingStatus = status.chargingStatus;
-                                    }
+                                        miniTiles.push({icon: api.exported.Icons.class.list()["road"], text: status.mileage + " km", colorDefault: api.themeAPI.constants().DARK_COLOR_KEY});
 
-                                    // Closed status
-                                    if (status.doorLockState == "UNLOCKED" || status.doorDriverFront != "CLOSED" || status.doorDriverRear != "CLOSED" || status.doorPassengerFront != "CLOSED" || status.doorPassengerRear != "CLOSED" || status.hood != "CLOSED" || status.windowDriverFront != "CLOSED" || status.windowDriverRear != "CLOSED" || status.windowPassengerFront != "CLOSED" || status.windowPassengerRear != "CLOSED") {
-                                        miniTiles.push({icon: api.exported.Icons.class.list()["unlock"], text: api.translateAPI.t("bmw.unlocked"), colorDefault: api.themeAPI.constants().OFF_COLOR_KEY});
-                                    } else {
-                                        miniTiles.push({icon: api.exported.Icons.class.list()["lock"], text: api.translateAPI.t("bmw.locked"), colorDefault: api.themeAPI.constants().DARK_COLOR_KEY});
-                                    }
-
-                                    miniTiles.push({icon: api.exported.Icons.class.list()["road"], text: status.mileage + " km", colorDefault: api.themeAPI.constants().DARK_COLOR_KEY});
-
-                                    // Mini tiles
-                                    const tileId = status.vin;
-                                    api.dashboardAPI.unregisterTile(tileId);
-                                    const tile = api.dashboardAPI.Tile(tileId, api.dashboardAPI.TileType().TILE_SUB_TILES, null, null, null, null, null, null, 0, 109, null, miniTiles, api.webAPI.Authentication().AUTH_USAGE_LEVEL);
-                                    api.dashboardAPI.registerTile(tile);
-                                }).catch((e) => {
-                                    api.exported.Logger.err(e.message);
-                                });
+                                        // Mini tiles
+                                        const tileId = status.vin;
+                                        api.dashboardAPI.unregisterTile(tileId);
+                                        const tile = api.dashboardAPI.Tile(tileId, api.dashboardAPI.TileType().TILE_SUB_TILES, null, null, null, null, (this.carPictures[vehicles[i].originalData.vin] ? this.carPictures[vehicles[i].originalData.vin] : null), null, 0, 109, null, miniTiles, api.webAPI.Authentication().AUTH_USAGE_LEVEL);
+                                        api.dashboardAPI.registerTile(tile);
+                                    }).catch((e) => {
+                                        api.exported.Logger.err(e.message);
+                                    });
+                                }
                             }
                         }
                     }).catch((e) => {
